ef335608c7d80e1cb82ff49772ae1a53588f143d
Papa Assane Diop <assane.diop@apporio.sn>
2025-12-25 22:13:32 +0000

    add structure filters

--- src/api/structure/structure.controller.ts
+++ src/api/structure/structure.controller.ts
@@ -1,4 +1,15 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, Res, HttpStatus, Query } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  Res,
+  HttpStatus,
+  Query,
+} from '@nestjs/common';
 import { StructureService } from './structure.service';
 import { CreateStructureDto } from './dto/create-structure.dto';
 import { UpdateStructureDto } from './dto/update-structure.dto';
@@ -8,78 +19,98 @@ export class StructureController {
   constructor(private readonly structureService: StructureService) {}
 
   @Post()
-  async create(@Body() createStructureDto: CreateStructureDto,@Res() res) {
+  async create(@Body() createStructureDto: CreateStructureDto, @Res() res) {
     try {
-      const structure=await this.structureService.create(createStructureDto)
-      return res.status(HttpStatus.CREATED).json({message:"Structure créée",data:structure})
+      const structure = await this.structureService.create(createStructureDto);
+      return res
+        .status(HttpStatus.CREATED)
+        .json({ message: 'Structure créée', data: structure });
     } catch (error) {
-      return res.status(error.status).json(error)
+      return res.status(error.status).json(error);
     }
   }
 
-@Get()
-async findAll(
-  @Res() res,
-  @Query('page') page: number = 1,
-  @Query('limit') limit: number = 10,
-  @Query('regionId') regionId?: string,
-  @Query('level') level?: string,
-  @Query('name') name?: string,
-) {
+  @Get()
+  async findAll(
+    @Res() res,
+    @Query('page') page: number = 1,
+    @Query('limit') limit: number = 10,
+    @Query('region') region?: string,
+    @Query('department') department?: string,
+    @Query('district') district?: string,
+    @Query('level') level?: string,
+    @Query('type') type?: string,
+    @Query('name') name?: string,
+  ) {
     try {
       const result = await this.structureService.findAll({
         page,
         limit,
-        regionId,
+        region,
+        department,
+        district,
         level,
-        name
+        type,
+        name,
       });
 
       return res.status(HttpStatus.OK).json({
-        message: "Liste des structures",
+        message: 'Liste des structures',
         data: result.data,
         pagination: {
           total: result.total,
           page: result.page,
           totalPages: result.totalPages,
-          limit
-        }
+          limit,
+        },
       });
     } catch (error) {
       return res.status(error.status || HttpStatus.INTERNAL_SERVER_ERROR).json({
-        message: error.message || "Erreur serveur"
+        message: error.message || 'Erreur serveur',
       });
     }
   }
 
-
   @Get(':id')
-  async findOne(@Param('id') id: string,@Res() res) {
+  async findOne(@Param('id') id: string, @Res() res) {
     try {
-      const structure=await this.structureService.findOne(id);
-      return res.status(HttpStatus.OK).json({message:`Structure ${id} récupérée`,data:structure})
+      const structure = await this.structureService.findOne(id);
+      return res
+        .status(HttpStatus.OK)
+        .json({ message: `Structure ${id} récupérée`, data: structure });
     } catch (error) {
-      return res.status(error.status).json(error)
+      return res.status(error.status).json(error);
     }
   }
 
   @Patch(':id')
-  async update(@Param('id') id: string, @Body() updateStructureDto: UpdateStructureDto,@Res() res) {
+  async update(
+    @Param('id') id: string,
+    @Body() updateStructureDto: UpdateStructureDto,
+    @Res() res,
+  ) {
     try {
-      const updated=await this.structureService.update(id, updateStructureDto);
-      return res.status(HttpStatus.OK).json({message:`Structure ${id} mise à jour`,data:updated})
+      const updated = await this.structureService.update(
+        id,
+        updateStructureDto,
+      );
+      return res
+        .status(HttpStatus.OK)
+        .json({ message: `Structure ${id} mise à jour`, data: updated });
     } catch (error) {
-      return res.status(error.status).json(error)
+      return res.status(error.status).json(error);
     }
   }
 
   @Delete(':id')
-  async remove(@Param('id') id: string,@Res() res) {
+  async remove(@Param('id') id: string, @Res() res) {
     try {
-      const deleted=await this.structureService.remove(id);
-      return res.status(HttpStatus.OK).json({message:`Structure ${id} supprimée`,data:deleted})
+      const deleted = await this.structureService.remove(id);
+      return res
+        .status(HttpStatus.OK)
+        .json({ message: `Structure ${id} supprimée`, data: deleted });
     } catch (error) {
-      return res.status(error.status).json(error)
+      return res.status(error.status).json(error);
     }
   }
 }
--- src/api/structure/structure.service.ts
+++ src/api/structure/structure.service.ts
@@ -1,92 +1,122 @@
 import { HttpException, Injectable } from '@nestjs/common';
 import { CreateStructureDto } from './dto/create-structure.dto';
 import { UpdateStructureDto } from './dto/update-structure.dto';
-import { Model } from 'mongoose';
+import mongoose, { Model } from 'mongoose';
 import { Structure } from './interfaces/structure.interface';
 import { InjectModel } from '@nestjs/mongoose';
 import logger from 'src/utils/logger';
 
 @Injectable()
 export class StructureService {
-  constructor(@InjectModel('Structure') private structureModel:Model<Structure>){}
+  constructor(
+    @InjectModel('Structure') private structureModel: Model<Structure>,
+  ) {}
   async create(createStructureDto: CreateStructureDto) {
     try {
-      const structure=await this.structureModel.create(createStructureDto)
-      return structure
+      const structure = await this.structureModel.create(createStructureDto);
+      return structure;
     } catch (error) {
-      logger.info(`---STRUCTURE.SERVICE.CREATE ERROR--- ${error}`)
-      throw new HttpException(error.message,error.status)
+      logger.info(`---STRUCTURE.SERVICE.CREATE ERROR--- ${error}`);
+      throw new HttpException(error.message, error.status);
     }
   }
 
   async findAll(query: {
     page?: number;
     limit?: number;
-    regionId?: string;
+    region?: string;
+    department?: string;
+    district?: string;
     level?: string;
     name?: string;
+    type?: string;
   }) {
     try {
-      const { page = 1, limit = 10, regionId, level, name } = query;
+      const {
+        page = 1,
+        limit = 10,
+        region,
+        department,
+        district,
+        level,
+        name,
+        type,
+      } = query;
 
       // Construction dynamique des filtres
       const filters: any = {};
-      if (regionId) filters.regionId = regionId;
-      if (level) filters.level = level;
-      if (name) filters.name = { $regex: name, $options: "i" }; // recherche insensible à la casse
-
+      if (region) filters.region = region;
+      if (department)
+        filters.department = new mongoose.Types.ObjectId(department) || null;
+      if (district)
+        filters.district = new mongoose.Types.ObjectId(district) || null;
+      if (level) filters.level = new mongoose.Types.ObjectId(level) || null;
+      if (name) filters.name = { $regex: name, $options: 'i' }; // recherche insensible à la casse
+      if (type) filters.type = type;
       // Pagination
       const skip = (page - 1) * limit;
 
       const [data, total] = await Promise.all([
-        this.structureModel.find(filters).skip(skip).limit(limit).sort({created_at:-1})
-        .populate('region', 'name code')
-        .populate('department', 'name')    
-        .populate('district', 'name')
-        .populate('level', 'name'),
-        this.structureModel.countDocuments(filters)
+        this.structureModel
+          .find(filters)
+          .skip(skip)
+          .limit(limit)
+          .sort({ created_at: -1 })
+          .populate('region', 'name code')
+          .populate('department', 'name')
+          .populate('district', 'name')
+          .populate('level', 'name'),
+        this.structureModel.countDocuments(filters),
       ]);
 
       return {
         data,
         total,
         page,
-        totalPages: Math.ceil(total / limit)
+        totalPages: Math.ceil(total / limit),
       };
     } catch (error) {
-      throw new HttpException(error.message || "Erreur serveur", error.status);
+      throw new HttpException(error.message || 'Erreur serveur', error.status);
     }
   }
 
-
   async findOne(id: string) {
     try {
-      const structure=await this.structureModel.findById(id)
-      .populate('region', 'name code')
-      .populate('department', 'name')    
-      .populate('district', 'name')
-      .populate('level', 'name')
-      return structure
+      const structure = await this.structureModel
+        .findById(id)
+        .populate('region', 'name code')
+        .populate('department', 'name')
+        .populate('district', 'name')
+        .populate('level', 'name');
+      return structure;
     } catch (error) {
-      throw new HttpException(error.message,error.status)
+      throw new HttpException(error.message, error.status);
     }
   }
 
   async update(id: string, updateStructureDto: UpdateStructureDto) {
     try {
-      const updated=await this.structureModel.findByIdAndUpdate(id,updateStructureDto,{new:true})
-      return updated
+      const updated = await this.structureModel.findByIdAndUpdate(
+        id,
+        updateStructureDto,
+        { new: true },
+      );
+      return updated;
     } catch (error) {
-      throw new HttpException(error.message,error.status)
+      throw new HttpException(error.message, error.status);
     }
   }
 
   async remove(id: string) {
     try {
-      const deleted=await this.structureModel.findByIdAndUpdate(id,{active:false},{new:true})
-      return deleted
+      const deleted = await this.structureModel.findByIdAndUpdate(
+        id,
+        { active: false },
+        { new: true },
+      );
+      return deleted;
     } catch (error) {
-      throw new HttpException(error.message,error.status)
+      throw new HttpException(error.message, error.status);
     }
   }
 }
